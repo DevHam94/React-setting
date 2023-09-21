14a856865b376dc25f6a3a089591b8105d5c578a
admin <gkaeogus7@gmail.com>
2023-09-21 13:58:36 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        compress: true,
+        port: 9999.
+    },
+        
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // 정규식으로 
+                exclude: /node_modules/, // 정규식으로 특정한 폴더를 제외시킨다. 
+                use: {
+                    loader: 'babel-loader',
+                    optoins: {
+                        presets: ["babel/presemt-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '2.3',
+            template: 'index.html'
+})
+    ]
+}
